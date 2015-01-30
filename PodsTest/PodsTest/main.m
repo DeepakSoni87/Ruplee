9790b60d881ac56903d82ca54e1efbe115b3bb83
deepak.soni <deepak.soni@tekcpu088.local>
2015-01-30 17:38:46 +0530

    Deepak : Initial Commit

---
+++ PodsTest/PodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodsTest
+//
+//  Created by deepak.soni on 1/30/15.
+//  Copyright (c) 2015 deepak.soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
